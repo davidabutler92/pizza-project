5ee0a82cff6717cce24aa32fa3c299ce6b15c14a
davidabutler92 <davidabutler92@yahoo.com>
2020-08-30 09:47:11 -0700

    add click function to place order button and console.log('CLICK ME') to cheeck functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 //UI logic 
 $(document).ready(function() {
-  $("#order-now").click(function(event) {
+  $("#order-now").click(event => {
     event.preventDefault();
     $("#customize-pizza-form").show();
+  });
+
+  $("#place-order-button").click(event => {
+    event.preventDefault();
+    console.log('CLICK ME');
   });
 });
